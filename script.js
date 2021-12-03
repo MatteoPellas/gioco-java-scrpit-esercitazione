f1f1f9c05243c4259c616420378cb1bd068fd95a
MatteoPellas <pellas.matteo@iisgalvanimi.edu.it>
2021-12-03 11:51:14 +0000

    utilizzo wasd

--- script.js
+++ script.js
@@ -4,9 +4,21 @@ function startGame() {
 }
 function updateGameArea() {
     myGameArea.wallpaper(wallpaper);
+    myGameArea.structure(structure1);
+    myGameArea.structure(structure2);
     redsquare.x+=redsquare.speedX;
     redsquare.y+=redsquare.speedY;
+    
     myGameArea.draw(redsquare);
+    
+    if (redsquare.x-10<structure1.x){
+        
+        vLeft=0
+        console.log("stop")
+    }
+
+
+    
 }
   
   
@@ -27,14 +39,19 @@ var myGameArea = {
     wallpaper: function(component){
         this.context.fillStyle=component.color;
         this.context.fillRect(component.x,component.y,component.width,component.height);
+    },
+    structure : function(component){
+        this.context.fillStyle=component.color;
+        this.context.fillRect(component.x,component.y,component.width,component.height);
+
     }
 
 }
 var redsquare={
-    width:20,
-    height:20,
-    x:10,
-    y:120,
+    width:50,
+    height:50,
+    x:300,
+    y:500,
     color: "green",
     speedX : 0,
     speedY : 0
@@ -46,30 +63,63 @@ var wallpaper={
     y:0,
     color: "#f1f1f1"
 }
-
+var structure1={
+    width:10,
+    height:600,
+    x:20,
+    y:0,
+    color: "black"
+}
+var structure2={
+    width:10,
+    height:600,
+    x:580,
+    y:0,
+    color: "black"
+}
 //movimenti
-let v=2
-function moveup() {
-    redsquare.speedY -= v;
-  }
-  
-  function movedown() {
-    redsquare.speedY += v;
-  }
-  
-  function moveleft() {
-    redsquare.speedX -= v;
-  }
-  
-  function moveright() {
-    redsquare.speedX += v;
-  }
+
+let v=5
+let vLeft=v
+
+document.addEventListener("keydown",(event)=>{
+    if (event.key=="a"){
+        redsquare.speedX = -vLeft;
+        console.log(redsquare)
+    }
+    if (event.key=="d"){
+        redsquare.speedX = v;
+    }
+    if (event.key=="w"){
+        redsquare.speedY = -v;
+    }
+    if (event.key=="s"){
+        redsquare.speedY = v;
+    }
+})
+document.addEventListener("keyup",(event)=>{
+    if (event.key=="a"){
+        redsquare.speedX = 0;
+        
+    }
+    if (event.key=="d"){
+        redsquare.speedX = 0;
+    }
+    if (event.key=="w"){
+        redsquare.speedY = 0;
+    }
+    if (event.key=="s"){
+        redsquare.speedY = 0;
+    }
+})
+
+
 
   function clearmove() {
-      console.log ("ciao")
-        redsquare.speedX -=1; 
-        redsquare.speedY = 0; 
+        redsquare.speedX =0; 
+        redsquare.speedY =0; 
 }
+
 
 
 
