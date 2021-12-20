90dae69a0f5505e7958a13ee8a0f063a6b029395
MatteoPellas <pellas.matteo@iisgalvanimi.edu.it>
2021-12-20 12:48:57 +0000

    animazioni

--- script.js
+++ script.js
@@ -1,50 +1,64 @@
 function startGame() {
     myGameArea.start();
-    myGameArea.draw(redsquare);
+    myGameArea.draw(animatedObject);
+    animatedObject.loadImages();
+    
 }
 function updateGameArea() {
-    myGameArea.wallpaper(wallpaper);
-   
     
-    if (redsquare.x<wallpaper.x+v && redsquare.speedX<0){
-        vLeft=0
-        redsquare.speedX=0
+    myGameArea.draw(wallpaper);
+    myGameArea.draw(ostacolo);
+    myGameArea.drawGameObject(animatedObject);
+    
+    if (pausa==true){
+        animatedObject.update();
     }
-    if (redsquare.x>wallpaper.x+1500-redsquare.width-v && redsquare.speedX>0){
+    
+    if (animatedObject.x<wallpaper.x+v && animatedObject.speedX<0){
+        vLeft=0
+        animatedObject.speedX=0
+    }
+    if (animatedObject.x>wallpaper.x+1500-animatedObject.width-v && animatedObject.speedX>0){
         vRight=0
-        redsquare.speedX=0
+        animatedObject.speedX=0
           
     }
-    if (redsquare.y>wallpaper.y+900-redsquare.width-v && redsquare.speedY>0){
-        gravity=0
-        redsquare.speedY=0
-        redsquare.y=850
+    if (animatedObject.y>wallpaper.y+900-animatedObject.width-v && animatedObject.speedY>0){
+        jump-=gravity
+        animatedObject.speedY=0
+        animatedObject.y=800
         
     }
-    if (redsquare.y<wallpaper.y+900-redsquare.width-v ){
-        redsquare.speedY+=1;
+    if (animatedObject.y<wallpaper.y+900-animatedObject.width-v ){
+        animatedObject.speedY+=1;
         
-        jump = 0
+        jump-=gravity
         
     }
     
     else{
         vLeft=v
         vRight=v
-        gravity=3
+        gravity=1
         jump=10
     }
+
     
-    redsquare.x+=redsquare.speedX;
-    redsquare.y+=redsquare.speedY;
-    myGameArea.draw(redsquare);
+    if (ostacolo.x<wallpaper.x){
+        ostacolo.x=1500
+    }
+    
+    ostacolo.x+=ostacolo.speedX;
+    ostacolo.y+=ostacolo.speedY;
+    
 }
 let v=5
 let vLeft=v
 let vDown=v
 let vRight=v
 let jump=10
-let gravity=3
+let gravity=1
+let pausa=false
   
 
 var myGameArea = {  
@@ -54,38 +68,42 @@ var myGameArea = {
         this.canvas.height = 900;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        this.interval = setInterval(updateGameArea, 30);
+        this.interval = setInterval(updateGameArea, 5);
     },
     draw : function(component){
         this.context.fillStyle=component.color;
+        
         this.context.fillRect(component.x,component.y,component.width,component.height);
     },
-    wallpaper: function(component){
-        this.context.fillStyle=component.color;
-        this.context.fillRect(component.x,component.y,component.width,component.height);
-    },
-    structure : function(component){
-        this.context.fillStyle=component.color;
-        this.context.fillRect(component.x,component.y,component.width,component.height);
-
+    
+    drawGameObject: function(gameObject) {
+        this.context.drawImage(
+          gameObject.image,
+          gameObject.x,
+          gameObject.y,
+          gameObject.width,
+          gameObject.height
+        );
     }
+    
 
 }
-var redsquare={
-    width:50,
-    height:50,
-    x:300,
-    y:400,
-    color: "green",
-    speedX : 0,
-    speedY : 0
-}
+
 var wallpaper={
     width:1500,
     height:900,
     x:0,
     y:0,
-    color: "#f1f1f1"
+    color: "green"
+}
+var ostacolo={
+    width:20,
+    height:100,
+    x:900,
+    y:800,
+    color: "red",
+    speedX: 0,
+    speedY: 0
 }
 
 
@@ -94,28 +112,70 @@ var wallpaper={
 
 
 document.addEventListener("keydown",(event)=>{
-    if (event.key=="a"){
-        redsquare.speedX = -vLeft;
-        
-    }
     if (event.key=="d"){
-        redsquare.speedX = vRight;
+        ostacolo.speedX = -vLeft;
+        pausa=true
+    }
+    if (event.key=="a"){
+        ostacolo.speedX = vRight;
+        pausa=true
     }
     if (event.key==" "){
-        redsquare.speedY = -jump;
+        animatedObject.speedY = -jump;
     }
    
 })
 document.addEventListener("keyup",(event)=>{
-    if (event.key=="a"){
-        redsquare.speedX = 0;
-        
-    }
     if (event.key=="d"){
-        redsquare.speedX = 0;
+        ostacolo.speedX = 0;
+        pausa = false
+    }
+    if (event.key=="a"){
+        ostacolo.speedX = 0;
+        pausa = false
     }
     if (event.key==" "){
-        redsquare.speedY = jump;
+        animatedObject.speedY = jump;
     }
    
 })
+
+var animatedObject = {
+    speedX: 0,
+    speedY: 0,
+    width: 100,
+    height: 100,
+    x: 10,
+    y: 120,
+    imageList: [], //Vettore che conterrà tutte le immagini caricate
+    contaFrame: 0, //Tiene conto di quanti frame sono passati
+    actualFrame: 0, //Specifica quale frame disegnare
+  
+    update: function() {
+      this.x += this.speedX;
+      this.y += this.speedY;
+      this.contaFrame++;
+      if (this.contaFrame == 20) {
+        this.contaFrame = 0;
+        this.actualFrame = (1 + this.actualFrame) % this.imageList.length;
+        
+        this.image = this.imageList[this.actualFrame];
+      }
+    },
+  
+    loadImages: function() {
+       for (imgPath of running) {
+        var img = new Image(this.width, this.height);
+        img.src = imgPath;
+        this.imageList.push(img);
+        //console.log(img);
+      }
+      this.image = this.imageList[this.actualFrame];
+    }
+  };
+  
+  
+ 
+  
+  
+  
---
+++ sprite.js
@@ -0,0 +1,10 @@
+var running = [
+    "png/Run (1).png",
+    "png/Run (2).png",
+    "png/Run (3).png",
+    "png/Run (4).png",
+    "png/Run (5).png",
+    "png/Run (6).png",
+    "png/Run (7).png",
+    "png/Run (8).png"
+];
